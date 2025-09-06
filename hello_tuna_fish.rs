d8a69a51cc77a01ca4e0d952fbb84bde9e8164f9
Rosie89757 <106412904+Nanastrike@users.noreply.github.com>
2025-09-06 21:38:33 +0000

    Complete programming languages assignment

---
+++ hello_tuna_fish.rs
@@ -0,0 +1,15 @@
+///
+  @author :Qinyu Luo
+  @python version: 3.12.1
+  @course: CST8002
+  @assignment: Practical Assessment 01
+  @time: 2025/9/6
+  @description:  writing a simple line of python, and using the 
+    proper comments and documentations
+  @References:
+///
+
+
+fn main() {
+    println!("Hello, World!");
+}
