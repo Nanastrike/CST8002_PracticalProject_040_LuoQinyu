3c4ce28a84fac8b952b4b1ed2b05f30c9a8b5def
Rosy_JR <silverluo24@gmail.com>
2025-09-21 12:35:10 -0400

    Create RecordObjects class with all getters and setters

---
+++ RecordObject.js
@@ -0,0 +1,59 @@
+class RecordObjects{
+    constructor(siteIdentification, year, transect, quadrat, speciesCommonName, count){
+        this.siteIdentification = siteIdentification;
+        this.year = year;
+        this.transect = transect;
+        this.quadrat = quadrat;
+        this.speciesCommonName = speciesCommonName;
+        this.count = count;
+    }
+
+    getSiteIdentification() {
+        return this.siteIdentification;
+    }
+
+    setSiteIdentification(siteIdentification) {
+        this.siteIdentification = siteIdentification;
+    }
+
+    getYear(){
+        return this.year;
+    }
+
+    setYear(year){
+        this.year = year;
+    }
+
+    getTransect(){
+        return this.transect;
+    }
+    
+    setTransect(transect){
+        this.transect = transect;
+    }
+
+    getQuadrat(){
+        return this.quadrat;
+    }
+
+    setQuadrat(quadrat){
+        return this.quadrat;
+    }
+
+    getSpeciesCommonName(){
+        return this.speciesCommonName;
+    }
+
+    setSpeciesCommonName(speciesCommonName){
+        this.speciesCommonName = speciesCommonName;
+    }
+
+    getCount(){
+        return this.count;
+    }
+
+    setCount(count){
+        this.count = count;
+    }
+}
+module.exports = RecordObjects;
