0e37499af53c61c545cecb976709ed96a33d03dc
Rosy_JR <silverluo24@gmail.com>
2025-10-10 13:25:39 -0400

    Add main application entry point

---
+++ Practical Project 02/index.js
@@ -0,0 +1,13 @@
+/**
+ * Main application entry point.
+ * Course: CST8002
+ * Professor: [Your Professor Name]
+ * Due Date: [Due Date]
+ * Author: [Your Full Name]
+ */
+
+import UserInterface from './presentation/UserInterface.js';
+
+// Start the application
+const ui = new UserInterface();
+ui.start();
